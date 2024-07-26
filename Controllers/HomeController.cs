14667c6941b883bb74b8ac741bade9ce8d943950
unknown <48716278@est.ort.edu.ar>
2024-07-26 18:07:59 -0300

    Worked on inputs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -36,12 +36,21 @@ public class HomeController : Controller
     public IActionResult Comenzar()
     {
         Escape.InicializarJuego();
+        ViewBag.estadoSalaID = 1;
         return View("habitaciones/habitacion1/habitacion11");
     }
 
     public IActionResult Habitacion(int sala)
     {
+        ViewBag.estadoSalaID = Escape.RevisarEstadoSala(sala);
         return View("habitaciones/habitacion" + sala.ToString().Substring(0, 1) + "/habitacion" + sala); //Va a la sala indicada por el parámetro
+    }
+
+    public IActionResult Resolver(string contrasena, int sala)
+    {
+        if (Escape.ResolverSala(contrasena)) Escape.AvanzarEstado();
+        ViewBag.estadoSalaID = Escape.RevisarEstadoSala(sala);
+        return View("habitaciones/habitacion1/habitacion12");
     }
 
     public IActionResult Privacy()
--- Models/Escape.cs
+++ Models/Escape.cs
@@ -3,6 +3,7 @@ static class Escape
     //Atributos
     public static string[] incognitasSalas {get; private set;}
     public static int estadoJuego = 1;
+    public static int contrasenaActual = 0;
     public static int[] estadoSalaID = new int[5];
     public static int[] posSalaID = new int[5];
 
@@ -10,34 +11,37 @@ static class Escape
     public static void InicializarJuego()
     {
         estadoJuego = 1;
+        contrasenaActual = 0;
         for(int i = 0; i < 5; i++)
         {
             estadoSalaID[i] = 1;
             posSalaID[i] = 1;
         }
-        string[] incognitasSalas = {"vivainfo", "462", "select contraseña from aulas where aula = 'AMI';", "173", "025358"}; //0 - 25 - 3 - 58
+        string[] incognitasSalas = {"vivainfo", "462", "select contraseña from aulas where aula = 'ami';", "173", "025358"}; //0 - 25 - 3 - 58
     }
     public static int GetEstadoJuego()
     {
         return estadoJuego;
     }
-    public static bool ResolverSala(int sala, string incognita)
+    public static void AvanzarEstado()
+    {
+        estadoJuego++;
+    }
+    public static bool ResolverSala(string incognita)
     {
         int estadoJuego = GetEstadoJuego();
-        if (sala == estadoJuego)
+        if (incognita == incognitasSalas[contrasenaActual])
         {
-            if (incognita == incognitasSalas[estadoJuego])
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            contrasenaActual++;
+            return true;
         }
-        else
-        {
-            return false;
-        }
+        else return false;
+    }
+    public static int RevisarEstadoSala(int sala)
+    {
+        sala /= 10;
+
+        //Acá es donde tengo que poner if (estadoJuego == sth) estadoSalaID[sth]++
+        return estadoSalaID[sala - 1];
     }
 }
